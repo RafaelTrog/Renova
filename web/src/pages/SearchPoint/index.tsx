6cdfdccc772026ec2a1236f79b0e578f32de29cc
Rafael Trog <rafael.trog@gmail.com>
2020-06-14 16:45:36 -0300

    adicionando busca

--- web/src/pages/SearchPoint/index.tsx
+++ web/src/pages/SearchPoint/index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState, FormEvent } from 'react';
+import React, { useEffect, useState, FormEvent, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import axios from 'axios';
 import api from '../../services/api';
 import './styles.css';
 import logo from '../../assets/logoRenova.png';
 import three from '../../assets/figura-three.png';
-import nav from '../../assets/nav-bg.png'; 
 
 interface Item {
     id: number;
@@ -13,15 +13,71 @@ interface Item {
     image_url: string;
 }
 
+interface Locales {
+    image: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    latitude: number;
+    longitude: number;
+    city: string;
+    uf: string;
+}
+
+interface IBGEUFResponse {
+    sigla: string;
+}
+
+interface IBGECityResponse {
+    nome: string;
+}
+
 const SearchPoint = () => {
     const [items, setItems] = useState<Item[]>([]);
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+    const [locales, setSearchLocales] = useState<Locales[]>([]);
+    
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
+    const [selectedUf, setSelectedUf] = useState('0');
+    const [selectedCity, setSelectedCity] = useState('0');
 
     useEffect(() => {
         api.get('items').then(response => {
             setItems(response.data);
         });
     }, []);
+
+    useEffect(() => {
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+            const ufInitials = response.data.map(uf => uf.sigla);
+
+            setUfs(ufInitials);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (selectedUf === '0') {
+            return;
+        }
+
+        axios
+            .get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+            .then(response => {
+                const cityNames = response.data.map(city => city.nome);
+
+            setCities(cityNames);
+            });
+    }, [selectedUf]);
+
+    useEffect(() => {
+        axios
+        .get<Locales[]>(`https://localhost:3333/points?city=${selectedCity}&uf=${selectedUf}&items=2`)
+        .then(response => {
+            setSearchLocales(response.data);
+        });
+    }, [setSelectedItems])
 
     
     function handleSelectItem(id: number) {
@@ -36,10 +92,33 @@ const SearchPoint = () => {
         }
     };
 
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+        const uf = event.target.value;
+
+        setSelectedUf(uf);
+    }
+    
+    function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
+        const city = event.target.value;
+
+        setSelectedCity(city);
+    }
+
     async function handleSubmit(event: FormEvent) {
         event.preventDefault();
+        const uf = selectedUf;
+        const city = selectedCity;
         const items = selectedItems;
-        console.log('EBAAA DEU CERTO');
+
+        const data = {
+            uf,
+            city,
+            items
+        };
+
+
+
+        console.log(setSearchLocales);
     }
     
     return (
@@ -57,6 +136,45 @@ const SearchPoint = () => {
         
         <form onSubmit={handleSubmit}>
             <h1>Busque um <br/> ponto de coleta</h1>
+
+            <fieldset>
+                <legend>
+                    <h2>Localidade</h2>
+                    <span>Selecione uma localidade</span>
+                </legend>
+
+                <div className="field-group">
+                    <div className="field">
+                        <label htmlFor="uf">Estado (UF)</label>
+                        <select
+                            name="uf"
+                            id="uf"
+                            value={selectedUf}
+                            onChange={handleSelectUf}
+                        >
+                            <option value="0">Selecione uma UF</option>
+                            {ufs.map(uf => (
+                                <option key={uf} value={uf}>{uf}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="city">Cidade</label>
+                        <select
+                            name="city"
+                            id="city"
+                            value={selectedCity}
+                            onChange={handleSelectCity}
+                        >
+                            <option value="0">Selecione uma cidade</option>
+                            {cities.map(city => (
+                                <option key={city} value={city}>{city}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+            </fieldset>
+
             <fieldset>
                 <legend>
                     <h2>Ítens de coleta</h2>
@@ -76,7 +194,10 @@ const SearchPoint = () => {
                         ))}
                     </ul>                                      
             </fieldset>
-            <button type="submit">Pesquisar</button>  
+
+            <button type="submit">
+                Pesquisar
+            </button>  
         </form>
     </div>
     );
